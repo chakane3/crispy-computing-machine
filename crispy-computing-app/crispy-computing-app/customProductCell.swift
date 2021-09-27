7b5a329a8caa0c35b6010585a3ef0eda83210d0c
chakane <chakanezshegog@gmail.com>
2021-09-27 08:16:34 -0400

    1. get image for shotgun LG 2. display UI 3. start on third site

--- crispy-computing-app/crispy-computing-app/customProductCell.swift
+++ crispy-computing-app/crispy-computing-app/customProductCell.swift
@@ -2,7 +2,27 @@
 //  customProductCell.swift
 //  crispy-computing-app
 //
-//  Created by Chakane Shegog on 9/19/21.
+//  Created by Chakane Shegog on 9/27/21.
 //
 
 import UIKit
+
+class customProductCell: UITableViewCell {
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // change the corner radius here
+        productImage.layer.cornerRadius = 4
+    }
+    
+    func configureCell(for product: productData) {
+        productNameLabel.text = product.name
+        productPriceLabel.text = product.price
+        
+        // TODO: add image here
+    }
+    
+}
---
+++ crispy-computing-app/crispy-computing-app/productSelectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  productSelectionViewController.swift
+//  crispy-computing-app
+//
+//  Created by Chakane Shegog on 9/27/21.
+//
+
+import UIKit
+
+class productSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
