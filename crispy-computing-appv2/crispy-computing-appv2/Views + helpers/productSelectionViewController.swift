62910cf40b7a1a86bf10b37405f0f93220269db5
chakane <chakanezshegog@gmail.com>
2021-10-13 19:50:17 -0400

    save for json dump

---
+++ crispy-computing-appv2/crispy-computing-appv2/Views + helpers/productSelectionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  crispy-computing-appv2
+//
+//  Created by Chakane Shegog on 10/11/21.
+//
+
+import UIKit
+
+// keep track of what products to display
+enum ammoOrArms {
+    case ammo
+    case arms
+}
+
+enum gunType {
+    case handgun
+    case rifle
+    case shotgun
+}
+
+class productSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        dump(result.getAmmo())
+    }
+
+
+}
+
